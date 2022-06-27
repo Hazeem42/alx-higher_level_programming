ee6f03e84ad6efb20a3da5b324699bca56bd7b76
Hazeem42 <oshoalakorede42@gmail.com>
2022-06-27 01:58:45 -0700

    Classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -25,8 +25,9 @@ class Square:
                 raise TypeError("size must be an integer")
             elif value < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = value
+            else:
+                self.__size = value
 
             def area(self):
                 """Return the current area of the square."""
-                return (self.__size ** self.__size)
+                return (self.__size * self.__size)
